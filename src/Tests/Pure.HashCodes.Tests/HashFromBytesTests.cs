678e20e68562ff05645c10240907498b64e9e946
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-29 11:44:11 +0300

    Implemented tests for HashFromBytes

---
+++ src/Tests/Pure.HashCodes.Tests/HashFromBytesTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+
+namespace Pure.HashCodes.Tests;
+
+public sealed record HashFromBytesTests
+{
+    [Fact]
+    public void ProduceDeterminedCode()
+    {
+        Random random = new Random();
+        byte[] bytes = Enumerable.Range(0, 1000).Select(_ => (byte)random.Next(0, 255)).ToArray();
+        Assert.True(SHA256.HashData(bytes).SequenceEqual(new HashFromBytes(bytes)));
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnEmptyCollection()
+    {
+        Assert.Throws<ArgumentException>(() => new HashFromBytes([]).ToArray());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new HashFromBytes([]).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new HashFromBytes([]).ToString());
+    }
+}
