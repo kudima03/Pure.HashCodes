3e93a30ff014f8db39ec2c6e323ded10c6fe7019
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-29 11:53:41 +0300

    Improved tests for HashFromBytes

--- src/Tests/Pure.HashCodes.Tests/HashFromBytesTests.cs
+++ src/Tests/Pure.HashCodes.Tests/HashFromBytesTests.cs
@@ -1,9 +1,59 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+using System.Security.Cryptography;
 
 namespace Pure.HashCodes.Tests;
 
 public sealed record HashFromBytesTests
 {
+    [Fact]
+    public void EnumeratesAsUntyped()
+    {
+        Random random = new Random();
+        byte[] bytes = Enumerable.Range(0, 1000).Select(_ => (byte)random.Next(0, 255)).ToArray();
+
+        byte[] expectedHash = SHA256.HashData(bytes);
+
+        IEnumerable<byte> actualHash = new HashFromBytes(bytes);
+
+        bool notEqual = false;
+        int index = 0;
+
+        foreach (object element in actualHash)
+        {
+            if ((byte)element != expectedHash[index++])
+            {
+                notEqual = true;
+                break;
+            }
+        }
+
+        Assert.False(notEqual);
+    }
+
+    [Fact]
+    public void EnumeratesAsTyped()
+    {
+        Random random = new Random();
+        byte[] bytes = Enumerable.Range(0, 1000).Select(_ => (byte)random.Next(0, 255)).ToArray();
+
+        byte[] expectedHash = SHA256.HashData(bytes);
+
+        IEnumerable<byte> actualHash = new HashFromBytes(bytes);
+
+        bool notEqual = false;
+
+        foreach ((byte element, int index) in actualHash.Select((element, index) => (element, index)))
+        {
+            if (element != expectedHash[index])
+            {
+                notEqual = true;
+                break;
+            }
+        }
+
+        Assert.False(notEqual);
+    }
+
     [Fact]
     public void ProduceDeterminedCode()
     {
