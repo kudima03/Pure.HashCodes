0b8087d153ae5fc2bb1ea9c0d14fee6a5b661ca3
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-29 21:23:06 +0300

    Implemented HashFromChar

---
+++ src/Pure.HashCodes/HashFromChar.cs
@@ -0,0 +1,36 @@
+﻿using Pure.Primitives.Abstractions.Char;
+using System.Collections;
+
+namespace Pure.HashCodes;
+
+public sealed record HashFromChar : IDeterminedHash
+{
+    private const byte typePrefix = 1;
+
+    private readonly IChar _value;
+
+    public HashFromChar(IChar value)
+    {
+        _value = value;
+    }
+
+    public IEnumerator<byte> GetEnumerator()
+    {
+        return new HashFromBytes(BitConverter.GetBytes(_value.CharValue).Prepend(typePrefix)).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
