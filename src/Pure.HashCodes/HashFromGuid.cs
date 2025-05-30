6829bdc7cb6e7723693f93ab0651b3fc72126d69
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-30 14:14:47 +0300

    Implemented HashFromGuid.

---
+++ src/Pure.HashCodes/HashFromGuid.cs
@@ -0,0 +1,36 @@
+﻿using Pure.Primitives.Abstractions.Guid;
+using System.Collections;
+
+namespace Pure.HashCodes;
+
+public sealed record HashFromGuid : IDeterminedHash
+{
+    private const byte typePrefix = 4;
+
+    private readonly IGuid _value;
+
+    public HashFromGuid(IGuid value)
+    {
+        _value = value;
+    }
+
+    public IEnumerator<byte> GetEnumerator()
+    {
+        return new HashFromBytes(_value.GuidValue.ToByteArray().Prepend(typePrefix)).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
