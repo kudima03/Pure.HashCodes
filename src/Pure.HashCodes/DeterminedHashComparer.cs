44a8ac5beea55bccd6a968796e8aca94e465b972
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-07 19:01:14 +0300

    Code formatted.

--- src/Pure.HashCodes/DeterminedHashComparer.cs
+++ src/Pure.HashCodes/DeterminedHashComparer.cs
@@ -13,10 +13,10 @@ internal sealed class DeterminedHashComparer : IComparer<IDeterminedHash>
 
         IImmutableList<byte> firstHashBytes = first!.ToImmutableArray();
         IImmutableList<byte> secondHashBytes = second!.ToImmutableArray();
-        
+
         return firstHashBytes.Zip(secondHashBytes,
                 (elementInFirst, elementInSecond) => elementInFirst.CompareTo(elementInSecond))
-            .FirstOrDefault(cmp => cmp != 0, 
+            .FirstOrDefault(cmp => cmp != 0,
                 firstHashBytes.Count.CompareTo(secondHashBytes.Count));
     }
 
