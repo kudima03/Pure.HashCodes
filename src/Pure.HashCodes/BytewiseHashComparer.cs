decca743088f529a574ad35aaf3c6abcd514c9d6
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-07 17:38:46 +0300

    Implemented OrderedHashes.

---
+++ src/Pure.HashCodes/BytewiseHashComparer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Immutable;
+
+namespace Pure.HashCodes;
+
+internal sealed class DeterminedHashComparer : IComparer<IDeterminedHash>
+{
+    public int Compare(IDeterminedHash? first, IDeterminedHash? second)
+    {
+        if (ReferenceEquals(first, second))
+        {
+            return 0;
+        }
+
+        IImmutableList<byte> firstHashBytes = first!.ToImmutableArray();
+        IImmutableList<byte> secondHashBytes = first!.ToImmutableArray();
+
+        return firstHashBytes.Zip(secondHashBytes,
+                (elementInFirst, elementInSecond) => elementInFirst.CompareTo(elementInSecond))
+            .FirstOrDefault(cmp => cmp != 0, 
+                firstHashBytes.Count.CompareTo(secondHashBytes.Count));
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
