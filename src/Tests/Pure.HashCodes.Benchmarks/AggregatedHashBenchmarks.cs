777cc78d917437a074280a0fc8eeb9dd69d0e7b8
Dmitry Kurochkin <kudima03@gmail.com>
2025-09-24 13:59:32 +0300

    Added benchmarks for AggregatedHash

---
+++ src/Tests/Pure.HashCodes.Benchmarks/AggregatedHashBenchmarks.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using Microsoft.VSDiagnostics;
+using Pure.HashCodes.Benchmarks.Fakes;
+
+namespace Pure.HashCodes.Benchmarks;
+
+[MemoryDiagnoser]
+[CPUUsageDiagnoser]
+public class AggregatedHashBenchmarks
+{
+    private readonly IEnumerable<IDeterminedHash> _hashes = [.. Enumerable
+        .Range(0, 1000)
+        .Select(_ => new FakeDeterminedHash())];
+
+    [Benchmark]
+    public int HashesMultiple()
+    {
+        return new AggregatedHash(_hashes).Count();
+    }
+}
