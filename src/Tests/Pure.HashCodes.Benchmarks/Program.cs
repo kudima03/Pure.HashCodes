8a9749fda62a6c89de66b0c7c7836e504584c734
Dmitry Kurochkin <kudima03@gmail.com>
2025-09-24 17:01:52 +0300

    Changed report collection.

--- src/Tests/Pure.HashCodes.Benchmarks/Program.cs
+++ src/Tests/Pure.HashCodes.Benchmarks/Program.cs
@@ -1,3 +1,8 @@
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 
-_ = BenchmarkRunner.Run(typeof(Program).Assembly);
+_ = BenchmarkRunner.Run(
+    typeof(Program).Assembly,
+    DefaultConfig.Instance.WithOptions(ConfigOptions.JoinSummary),
+    args
+);
